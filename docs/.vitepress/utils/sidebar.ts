b9453fa7a1c58e90d2394283034b50eb0e868c5b
txtxj <1418373194@qq.com>
2022-09-09 19:05:00 +0800

    update sidebar

--- docs/.vitepress/utils/sidebar.ts
+++ docs/.vitepress/utils/sidebar.ts
@@ -4,7 +4,8 @@ export const sidebar = {
       text: '实验',
       link: '/labs/index',
       items: [
-        { text: 'C1：大素数', link: '/labs/c1' },
+        { text: 'C1：大素数生成', link: '/labs/c1' },
+        { text: 'C2：游戏或工具', link: '/labs/c2' },
       ],
     },
   ],
