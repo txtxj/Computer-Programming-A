2e342b927f85094839526fb4aba2462d9125704a
txtxj <1418373194@qq.com>
2022-09-20 19:08:49 +0800

    add group

--- docs/.vitepress/utils/sidebar.ts
+++ docs/.vitepress/utils/sidebar.ts
@@ -4,6 +4,7 @@ export const sidebar = {
       text: '实验',
       link: '/labs/index',
       items: [
+        { text: '作业 A 分组', link: '/labs/group' },
         { text: 'A1：基本数据类型', link: '/labs/a1' },
         { text: 'C1：大素数生成', link: '/labs/c1' },
         { text: 'C2：游戏或工具', link: '/labs/c2' },
