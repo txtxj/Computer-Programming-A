356e0c022fc76be8c7a77d4f6e7b46ba9956c98d
txtxj <1418373194@qq.com>
2022-09-25 22:24:38 +0800

    finish a2

--- docs/.vitepress/utils/sidebar.ts
+++ docs/.vitepress/utils/sidebar.ts
@@ -6,7 +6,7 @@ export const sidebar = {
       items: [
         { text: '作业 A 分组', link: '/labs/group' },
         { text: 'A1：基本数据类型', link: '/labs/a1' },
-        { text: 'A2：结构化编程练习', link: '/labs/a2' },
+        { text: 'A2：编程练习', link: '/labs/a2' },
         { text: 'C1：大素数生成', link: '/labs/c1' },
         { text: 'C2：游戏或工具', link: '/labs/c2' },
       ],
